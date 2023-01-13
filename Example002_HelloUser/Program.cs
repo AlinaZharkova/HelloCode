ae73581faaa36a680ba250cf9826408b679a7d16
AlinaZharkova <alinka--91@mail.ru>
2023-01-13 22:33:49 +0300

    add Example 005 Condition If Else

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.Write("Введите Ваше имя");
-string Username = Console.ReadLine();
+﻿Console.Write("Введите Ваше имя: ");
+string? Username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.Write(Username);
 
